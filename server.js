235de96b3f25cc0c811c70ce409eae2cd5a6d703
hemant-chicmic <hemant.singh@chicmic.co.in>
2024-08-16 09:47:58 +0530

    add something

--- server.js
+++ server.js
@@ -1,36 +1,37 @@
 
 
-import express from  'express' ; 
-import dotenv from "dotenv" ; 
+import express from 'express';
+import { createServer } from 'node:http';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url)) ;
 
 
 
-
-dotenv.config() ; 
-
-const PORT = process.env.PORT || 3000 ;
-const app = express() ; 
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname , 'index.html' ) ) ; 
+});
 
 
-
-app.listen( PORT , () => {
-    console.log( ` Server is running on http://localhost:${PORT}` ) ; 
-} )
-
-
-
-
-
-
-
-app.get( '/' , (req , res) => {
-    res.status(200).json( {
-        message : "Suceessfully fetched details"
-    } )
-} )
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log( msg ) ; 
+      io.emit('chat message', msg);
+    });
+});
+  
+  
 
 
-
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
 
 
 
